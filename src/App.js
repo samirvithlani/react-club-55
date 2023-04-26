28f8bd83b44d8b9f6234d5940235ad5f1b6f0530
samirvithlani <samir.vithlani83955@gmail.com>
2023-04-26 17:32:19 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Users } from "./Users";
+import Footer from "./Footer";
+import { Header } from "./Header";
 
 function App() {
+  var a = 10;
+  var sName = "Sachin";
+  var salary = 12345.67;
+  var isIndian = true;
+
+  var user = {
+    name: "raj",
+    email: "raj@gmail.com",
+    mobile: "1234567890",
+  };
+
+  var students = [
+    {
+      id: 1,
+      name: "Raj",
+      email: "raj@gmail.com",
+    },
+    {
+      id: 2,
+      name: "jay",
+      email: "jay@gmail.com",
+    },
+    {
+      id: 3,
+      name: "Amit",
+      email: "Amit@gmail.com",
+    },
+  ];
+
+  //jsx
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Header/>
+        <Users/>
+        <h1>HEllo</h1>
+        <h1>{sName}</h1>
+        <h2>{salary}</h2>
+        <h3>{isIndian ? "Indian" : "Not Indian"}</h3>
+        <h1>Name = {user.name}</h1>
+        <h2>email : {user.email}</h2>
+        <h3>Mobile :-{user.mobile}</h3>
+        <div className="App">
+          <h1>Student Details</h1>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((s) => {
+                return (
+                  <tr>
+                    <td>{s.id}</td>
+                    <td>{s.name}</td>
+                    <td>{s.email}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <Footer/>
+      </div>
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Footer() {
+    var footerStyle = {
+        position: "relative",
+        top: "10vh",
+        width: "100%",
+        border: "2px solid red",
+        backgroundColor: "black",
+        color: "white",
+
+    }
+  return (
+    <div style={footerStyle}>
+        <h1>Footer</h1>
+    </div>
+  )
+}
